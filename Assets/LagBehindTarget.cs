9776c231fb326f4117320d7ee4712788d3d34042
Canaguia <filipgunnarkarlsson@gmail.com>
2024-10-05 16:37:13 +0200

    Updated PlayerController

---
+++ Assets/LagBehindTarget.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LagBehindTarget : MonoBehaviour
+{
+    [SerializeField] Transform m_followTarget;
+    [SerializeField] Transform m_animateTarget;
+    [SerializeField] float m_followSpeed = 1f;
+    [SerializeField] float m_rotateSpeed = 1f;
+
+    private void Start()
+    {
+        if (m_animateTarget == null) {
+            m_animateTarget = transform;
+        }
+    }
+
+    void Update()
+    {
+        m_animateTarget.transform.position = Vector3.Lerp(m_animateTarget.position, m_followTarget.position, Time.deltaTime * m_followSpeed);
+        m_animateTarget.rotation = Quaternion.Lerp(m_animateTarget.rotation, m_followTarget.rotation, Time.deltaTime * m_rotateSpeed);
+    }
+}
